228e91f420ade31d0600c067864f61e11e316eb7
mingqing <tanmq1995@outlook.com>
2023-10-04 19:37:11 +0800

    demo added

---
+++ src/main/java/com/mingqing/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.mingqing.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping("/world")
+	public String hello() {
+		System.out.println("hello world!");
+		return "Hello";
+	}
+}
